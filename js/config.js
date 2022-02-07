9f5519997ab396999daa1e1caa6a7458cbfa4e1f
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-02-07 18:45:17 +0100

    reconfigurar config.js
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- js/config.js
+++ js/config.js
@@ -1,17 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyBRmrHzs6LJtFihCfL81Ecty_V6uHLXb6A",
     authDomain: "d-gram-3331d.firebaseapp.com",
     projectId: "d-gram-3331d",
     storageBucket: "d-gram-3331d.appspot.com",
     messagingSenderId: "132602464174",
     appId: "1:132602464174:web:18d1397b751b02c431aa3f"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const auth = firebase.auth();
