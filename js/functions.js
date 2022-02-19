5c4648040897e43ff3055ce29c19c1e307acdc77
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-02-19 17:01:59 +0100

    Eliminar funcion showAlert (No se usa) Añadir funciones getUsername, getUrl y addPost

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,35 @@
-function showAlert(text, type) {
-    document.getElementById("alert").innerText = text;
-    document.getElementById("alert").className = "alert " + type;
-    document.getElementById("alert").style.display = "block";
-    window.setTimeout(function() {
-        document.getElementById("alert").style.display = "none";
-    }, 2000);
+function getUsername(id) {
+    return new Promise((resolve,reject)=>{
+        db.collection("users").doc(id)
+            .get()
+            .then((userobj) => {resolve(userobj.data().username);})
+            .catch((error) => {reject(error);})
+    });}
+
+function getURL(directory) {
+    return new Promise((resolve,reject)=>{
+        storageRef.child(directory).getDownloadURL()
+            .then((url) => {resolve(url);})
+            .catch((error) => {reject(error);})
+    })}
+
+function addPost(postobject, post_table) {
+    return new Promise((resolve)=>{
+        getUsername(postobject.data().user.id)
+            .then(user => {
+                getURL(postobject.data().image)
+                    .then(image_url => {
+                        getURL(postobject.data().design)
+                            .then(design_url => {
+                                let username = user;
+                                let title = postobject.data().title;
+                                let description = postobject.data().description;
+                                let image = image_url;
+                                let design = design_url;
+                                document.getElementById(post_table).innerHTML += `<tr><td><div class="post"><h2 class="usuario">${username}</h2><div class="caja"><div class="fotos"><img src="${image}" width="100%" height="100%" alt="${title}"/><div class="titulo">${title}</div><div class="descripcion">${description}</div></div></div><div class="abajo"><div class="likes"><div class="contenido"><span class="corazon"></span><span class="like"></span><span class="numero"></span></div></div></div></div></td></tr>`
+                                resolve();
+                            })
+                    })
+            })
+    })
 }
